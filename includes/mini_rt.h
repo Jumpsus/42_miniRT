ad0f6991221565d03ea08d0d246f2d498234cd93
Jumpsus <peerapat.rs@gmail.com>
2022-12-13 20:51:48 +0700

    add function in header file

--- includes/mini_rt.h
+++ includes/mini_rt.h
@@ -58,4 +58,16 @@ typedef struct s_main {
 	t_object	*obj;
 }	t_main;
 
+t_point     create_vector(double x, double y, double z);
+t_point		vector_add(t_point a, t_point b);
+t_point		vector_subtract(t_point a, t_point b);
+t_point		vector_multiply(t_point a, double multiplier);
+t_point		vector_divide(t_point a, double divisor);
+double		vector_square_length(t_point a);
+double		vector_length(t_point a);
+double		vector_dot(t_point a, t_point b);
+t_point		vector_cross(t_point a, t_point b);
+t_point		unit_vector(t_point a);
+t_point		rotate_vector(t_point v, double x_rot, double y_rot, double z_rot);
+
 #endif

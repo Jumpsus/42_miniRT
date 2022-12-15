432b0db231aa75457d127bc39ab980ba4e97fa0f
Jumpsus <peerapat.rs@gmail.com>
2022-12-15 23:31:25 +0700

    delete main

--- srcs/utils_matrix_1.c
+++ srcs/utils_matrix_1.c
@@ -49,28 +49,3 @@ t_point    vector_divide(t_point a, double divisor)
     return (vector_multiply(a, (1 / divisor)));
 }
 
-// int main() {
-//     t_point p1;
-//     t_point p2;
-//     t_point plus;
-//     t_point minus;
-//     t_point multiply;
-//     t_point divider;
-
-//     p1.x = 1;
-//     p1.y = 2;
-//     p1.z = 3;
-
-//     p2.x = 5;
-//     p2.y = 5;
-//     p2.z = 5;
-
-//     plus = vector_add(p1, p2);
-//     minus = vector_subtract(p1, p2);
-//     multiply = vector_multiply(p1, 10);
-//     divider = vector_divide(p1, 10);
-//     printf("%f %f %f \n", plus.x, plus.y, plus.z);
-//     printf("%f %f %f \n", minus.x, minus.y, minus.z);
-//     printf("%f %f %f \n", multiply.x, multiply.y, multiply.z);
-//     printf("%f %f %f \n", divider.x, divider.y, divider.z);
-// }
